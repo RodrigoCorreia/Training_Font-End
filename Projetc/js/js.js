059ab428c4a1769c7434e44f5580f73d111593ee
Rodrigo Correia <rodrigocorrei@gmail.com>
2018-03-08 13:49:36 -0300

    Add Carousel and video

--- Projetc/js/js.js
+++ Projetc/js/js.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
-    console.log("teste");
 
     $("#input-search").on("focus", function(){
+
         $("li.search").addClass("ativo");
+
     }).on("blur", function(){
+
         $("li.search").removeClass("ativo");
+
     });
 
 });
