0ceb3fbde3bf356973eb3d75f814509bb5efb044
Rodrigo Correia <rodrigocorrei@gmail.com>
2018-03-08 20:58:05 -0300

    Ajust sometings

--- Projetc/js/js.js
+++ Projetc/js/js.js
@@ -1,12 +1,35 @@
 $(document).ready(function(){
 
-    $("#input-search").on("focus", function(){
+    $(".thumbnails .item").on("click", function(){
 
-        $("li.search").addClass("ativo");
+        $("video").attr({
+            "src":"mp4/"+$(this).data('video')+".mp4",
+            "poster":"img/"+$(this).data('video')+".jpg"
+        });
 
-    }).on("blur", function(){
+    });
 
-        $("li.search").removeClass("ativo");
+    $("#volume").on("mousemove", function(){
+
+        $("video")[0].volume = parseFloat($(this).val());
+
+    });
+
+    $("#btn-play-pause").on("click", function(){
+
+
+
+        var video = $("video")[0];
+
+        if ($(this).hasClass("btn-success")) {
+            $(this).text("STOP");
+            video.play();
+        } else {
+            $(this).text("PLAY");
+            video.pause();
+        }
+
+        $(this).toggleClass("btn-success btn-danger");
 
     });
 
