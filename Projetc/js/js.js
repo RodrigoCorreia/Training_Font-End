4aea2fed5008e66540a93954b78c3e27bc693d13
Rodrigo Correia <rodrigocorrei@gmail.com>
2018-03-08 23:13:27 -0300

    Add a responsive site

--- Projetc/js/js.js
+++ Projetc/js/js.js
@@ -1,35 +1,73 @@
 $(document).ready(function(){
 
-    $(".thumbnails .item").on("click", function(){
+	$("#logotipo").on("mouseover",function(){
+		
+		$("#banner h1").addClass("efeito");
 
-        $("video").attr({
-            "src":"mp4/"+$(this).data('video')+".mp4",
-            "poster":"img/"+$(this).data('video')+".jpg"
-        });
+	}).on("mouseout", function(){
+
+		$("#banner h1").removeClass("efeito");
+
+	});
+
+	$("#input-search").on("focus", function(){
+
+		$("li.search").addClass("ativo");
+
+	}).on("blur", function(){
+
+		$("li.search").removeClass("ativo");				
+
+	});
+
+	$(".thumbnails").owlCarousel({
+ 
+      //autoPlay: 3000,
+      items : 4
+ 
+  	});
+
+  	var owl = $(".thumbnails").data('owlCarousel');
+
+  	$('#btn-news-prev').on("click", function(){
+
+  		owl.prev();
+
+  	});
+
+  	$('#btn-news-next').on("click", function(){
+
+  		owl.next();
+
+  	});
+
+  	$("#page-up").on("click", function(event){
+
+  		$("body").animate({
+  			scrollTop:0
+  		}, 1000);
+
+  		event.preventDefault();
+
+  	});
+
+    $("#btn-bars").on("click", function(){
+
+      $("header").toggleClass("open-menu");
 
     });
 
-    $("#volume").on("mousemove", function(){
+    $("#menu-mobile-mask, .btn-close").on("click", function(){
 
-        $("video")[0].volume = parseFloat($(this).val());
+      $("header").removeClass("open-menu");
+
 
     });
 
-    $("#btn-play-pause").on("click", function(){
+    $("#btn-search").on("click", function(){
 
-
-
-        var video = $("video")[0];
-
-        if ($(this).hasClass("btn-success")) {
-            $(this).text("STOP");
-            video.play();
-        } else {
-            $(this).text("PLAY");
-            video.pause();
-        }
-
-        $(this).toggleClass("btn-success btn-danger");
+      $("header").toggleClass("open-search");
+      $("#input-search-mobile").focus();
 
     });
 
